37119594f95fc8aabcd0e8e086638911bf5d6f1e
RupeshY <crrupesh0@gmail.com>
2026-07-12 13:20:23 +0545

    fromFirestore parses likeBy list and computes

--- test/models/comment_model_test.dart
+++ test/models/comment_model_test.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:conexus/models/comment_model.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -18,6 +20,25 @@ void main() {
       expect(map['authorId'], 'u1');
       expect(map['text'], 'nice Post!');
       expect(map['likedBy'], isEmpty);
+    });
+    test('fromFirestore parses likedBy list and computes likeCount', () async {
+      final firestore = FakeFirebaseFirestore();
+      final docRef = await firestore.collection('comments').add({
+        'postId': 'p1',
+        'authorId': 'u1',
+        'authorUsername': 'rupesh',
+        'authorPhotoUrl': '',
+        'text': 'hello',
+        'createdAt': Timestamp.now(),
+        'likedBy': ['u2', 'u3'],
+      });
+
+      final snapshot = await docRef.get();
+      final comment = CommentModel.fromFirestore(snapshot);
+
+      expect(comment.likeCount, 2);
+      expect(comment.isLikedBy('u2'), isTrue);
+      expect(comment.isLikedBy('u9'), isFalse);
     });
   });
 }
