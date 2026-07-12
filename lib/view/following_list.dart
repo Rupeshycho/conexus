d47256aeee7fd3455d570dd2f9ec22ceea9c2d74
SANDIP RAWAL <sandiprawal86@gmail.com>
2026-07-12 17:48:43 +0545

    Implement following list with profile navigation

--- lib/view/following_list.dart
+++ lib/view/following_list.dart
@@ -1,12 +1,43 @@
+import 'package:conexus/viewmodel/user_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'other_profile_screen.dart';
 
-class FollowingList extends StatelessWidget {
+class FollowingList extends StatefulWidget {
   final List<String> following;
 
-  const FollowingList({
-    super.key,
-    required this.following,
-  });
+  const FollowingList({super.key, required this.following});
+
+  @override
+  State<FollowingList> createState() => _FollowingListState();
+}
+
+class _FollowingListState extends State<FollowingList> {
+  Map<String, String> userNames = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserNames();
+  }
+
+  Future<void> _loadUserNames() async {
+    final userVM = context.read<UserViewModel>();
+    Map<String, String> names = {};
+
+    for (String uid in widget.following) {
+      try {
+        final user = await userVM.getUser(uid);
+        names[uid] = user.name.isEmpty ? user.username : user.name;
+      } catch (e) {
+        names[uid] = "Unknown User";
+      }
+    }
+
+    setState(() {
+      userNames = names;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +50,7 @@ class FollowingList extends StatelessWidget {
         backgroundColor: Colors.deepOrange,
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: following.isEmpty
+      body: widget.following.isEmpty
           ? const Center(
         child: Text(
           "Not Following Anyone",
@@ -27,17 +58,26 @@ class FollowingList extends StatelessWidget {
         ),
       )
           : ListView.builder(
-        itemCount: following.length,
+        itemCount: widget.following.length,
         itemBuilder: (context, index) {
+          final uid = widget.following[index];
+          final name = userNames[uid] ?? "Loading...";
+
           return ListTile(
             leading: const CircleAvatar(
               backgroundColor: Colors.deepOrange,
-              child: Icon(
-                Icons.person,
-                color: Colors.white,
-              ),
+              child: Icon(Icons.person, color: Colors.white),
             ),
-            title: Text(following[index]),
+            title: Text(name),
+            subtitle: Text("@$uid"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => OtherProfileScreen(userId: uid),
+                ),
+              );
+            },
           );
         },
       ),
