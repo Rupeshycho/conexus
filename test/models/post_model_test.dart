1571141c5d9ee88e2b0800e4c7227acbb547aefe
RupeshY <crrupesh0@gmail.com>
2026-07-12 12:25:12 +0545

    expect(map[]) added

--- test/models/post_model_test.dart
+++ test/models/post_model_test.dart
@@ -1,3 +1,6 @@
+import 'package:conexus/models/post_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
   group('PostModel', () {
     test('toMap contatins correct keys and valuess ', () {
@@ -11,6 +14,15 @@ void main() {
         caption: 'hello world',
         createdAt: DateTime.now(),
       );
+      final map = post.toMap();
+
+      expect(map['authorId'], 'u1');
+      expect(map['authorUsername'], 'rupesh');
+      expect(map['type'], 'image');
+      expect(map['mediaUrl'], 'https://res.cloudinary.com/demo/image.jpg');
+      expect(map['caption'], 'hello world');
+      expect(map['likeCount'], 0);
+      expect(map['commentCount'], 0);
     });
   });
 }
