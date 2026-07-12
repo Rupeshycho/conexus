5e9fabd915c9a7e48f1a4769b3c18b3d707098fb
RupeshY <crrupesh0@gmail.com>
2026-07-12 14:55:35 +0545

    addcomment save the comments and incerase post count

--- test/repo/comment_repo_impl_test.dart
+++ test/repo/comment_repo_impl_test.dart
@@ -1,5 +1,8 @@
 import 'package:conexus/models/notification_model.dart';
+import 'package:conexus/repo/comment_repo_impl.dart';
 import 'package:conexus/repo/notification_repo.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 class FakeNotificationRepo implements NotificationRepo {
   final List<Map<String, dynamic>> createdNotifications = [];
@@ -27,4 +30,43 @@ class FakeNotificationRepo implements NotificationRepo {
 
   @override
   Future<void> markAsRead(String notificationId) async {}
+}
+
+void main() {
+  late FakeFirebaseFirestore firestore;
+  late FakeNotificationRepo fakeNotificationRepo;
+  late CommentRepoImpl repo;
+
+  setUp(() {
+    firestore = FakeFirebaseFirestore();
+    fakeNotificationRepo = FakeNotificationRepo();
+    repo = CommentRepoImpl(
+      notificationRepo: fakeNotificationRepo,
+      firestore: firestore,
+    );
+  });
+  group('CommentRepoImpl', () {
+    test(
+      'addComment saves the comment and increaments post commnetCount',
+      () async {
+        final postRef = await firestore.collection('posts').add({
+          'authorId': 'owner1',
+          'commentCount': 0,
+        });
+        await repo.addComment(
+          postId: postRef.id,
+          authorId: 'commenter1',
+          authorUsername: 'jack',
+          authorPhotoUrl: '',
+          text: 'nice!',
+        );
+        final comments = await firestore.collection('comments').get();
+        expect(comments.docs.length, 1);
+        expect(comments.docs.first['text'], 'nice!');
+
+        final postSnap = await postRef.get();
+        expect(postSnap['commentCount'], 1);
+      },
+    );
+  });
 }
