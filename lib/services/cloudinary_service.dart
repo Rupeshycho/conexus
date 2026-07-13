940f5b7d8bc9eab985fc67968cc907df3ba38f36
SANDIP RAWAL <sandiprawal86@gmail.com>
2026-07-13 14:00:14 +0545

    cloudinary_service for image upload in profile

--- lib/services/cloudinary_service.dart
+++ lib/services/cloudinary_service.dart
@@ -1,17 +1,39 @@
+import 'dart:convert';
 import 'dart:io';
-import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:http/http.dart' as http;
 
 class CloudinaryService {
-  final CloudinaryPublic cloudinary = CloudinaryPublic(
-    'di3lnc0c3',
-    'conexus_upload',
-    cache: false,
-  );
+  static const String cloudName = "di3lnc0c3";
+  static const String uploadPreset = "conexus_upload";
 
-  Future<String> uploadImage(File image) async {
-    final response = await cloudinary.uploadFile(
-      CloudinaryFile.fromFile(image.path),
+  static Future<String?> uploadFile(
+      File file, {
+        required String resourceType,
+      }) async {
+    final uri = Uri.parse(
+      "https://api.cloudinary.com/v1_1/$cloudName/$resourceType/upload",
     );
-    return response.secureUrl;
+
+    final request = http.MultipartRequest("POST", uri)
+      ..fields["upload_preset"] = uploadPreset
+      ..files.add(await http.MultipartFile.fromPath("file", file.path));
+
+    final response = await request.send();
+
+    if (response.statusCode == 200) {
+      final body = await response.stream.bytesToString();
+      final json = jsonDecode(body);
+      return json["secure_url"] as String?;
+    }
+
+    return null;
+  }
+
+  static Future<String?> uploadImage(File file) async {
+    return uploadFile(file, resourceType: "image");
+  }
+
+  static Future<String?> uploadVideo(File file) async {
+    return uploadFile(file, resourceType: "video");
   }
 }
