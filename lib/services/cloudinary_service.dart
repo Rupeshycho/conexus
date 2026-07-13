d662869c45cfbf3218672011130c4f49b6ceb4cb
RupeshY <crrupesh0@gmail.com>
2026-07-13 14:04:39 +0545

    cloudinary services added

--- lib/services/cloudinary_service.dart
+++ lib/services/cloudinary_service.dart
@@ -1,57 +1,40 @@
-// lib/services/cloudinary_service.dart
 import 'dart:convert';
 import 'dart:io';
+
 import 'package:http/http.dart' as http;
 
 class CloudinaryService {
   static const String cloudName = "di3lnc0c3";
   static const String uploadPreset = "conexus_upload";
 
-  static Future<String> uploadImage(File file) async {
+  static Future<String?> uploadFile(
+    File file, {
+    required String resourceType,
+  }) async {
     final uri = Uri.parse(
-      "https://api.cloudinary.com/v1_1/$cloudName/image/upload",
-    );
-    return _upload(uri, file);
-  }
-
-  static Future<String> uploadVideo(File file) async {
-    final uri = Uri.parse(
-      "https://api.cloudinary.com/v1_1/$cloudName/video/upload",
-    );
-    return _upload(uri, file);
-  }
-
-  static Future<String> _upload(Uri uri, File file) async {
-    var request = http.MultipartRequest("POST", uri);
-    request.fields["upload_preset"] = uploadPreset;
-    request.files.add(
-      await http.MultipartFile.fromPath("file", file.path),
+      "https://api.cloudinary.com/v1_1/$cloudName/$resourceType/upload",
     );
 
-    try {
-      var response = await request.send();
+    final request = http.MultipartRequest("POST", uri)
+      ..fields["upload_preset"] = uploadPreset
+      ..files.add(await http.MultipartFile.fromPath("file", file.path));
 
-      if (response.statusCode == 200) {
-        var body = await response.stream.bytesToString();
-        var json = jsonDecode(body);
-        return json["secure_url"];
-      } else {
-        var body = await response.stream.bytesToString();
-        throw MediaUploadException(
-          "Upload failed (${response.statusCode}): $body",
-        );
-      }
-    } catch (e) {
-      if (e is MediaUploadException) rethrow;
-      throw MediaUploadException("Upload failed: $e");
+    final response = await request.send();
+
+    if (response.statusCode == 200) {
+      final body = await response.stream.bytesToString();
+      final json = jsonDecode(body);
+      return json["secure_url"] as String?;
     }
+
+    return null;
   }
-}
 
-class MediaUploadException implements Exception {
-  final String message;
-  MediaUploadException(this.message);
+  static Future<String?> uploadImage(File file) async {
+    return uploadFile(file, resourceType: "image");
+  }
 
-  @override
-  String toString() => message;
+  static Future<String?> uploadVideo(File file) async {
+    return uploadFile(file, resourceType: "video");
+  }
 }
