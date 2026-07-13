a15d9ed5d5bc6a262c1a485dc52313dbc3667cc7
unknown <ujwalacharya74@gmail.com>
2026-07-13 13:52:17 +0545

    adding repo files

--- lib/repo/user_repo.dart
+++ lib/repo/user_repo.dart
@@ -1,19 +1,30 @@
 import 'package:conexus/model/user_model.dart';
+
 abstract class UserRepo {
-  Future <String> login(String email,String password);
-  Future <String> register(String email,String password);
-  Future <void> logout();
-  Future <void> forgetPassword(String email);
+  // Auth
+  Future<String> login(String email, String password);
+  Future<String> register(String email, String password);
+  Future<void> logout();
+  Future<void> forgetPassword(String email);
 
-  Future <void> addUser(UserModel userModel);
-  Future <void>deleteUser(String id);
+  // CRUD
+  Future<void> addUser(UserModel userModel);
+  Future<void> deleteUser(String id);
+  Future<List<UserModel>> getAllUser();
+  Future<UserModel> getUserId(String id);
+  Future<void> editProfile(UserModel userModel);
+  Future<String> uploadProfileImage(String uid, String imagePath);
 
-  Future <List<UserModel>> getAllUser();
-  Future <UserModel> getUserId(String id);
-  Future <void> editProfile(UserModel userModel);
-  Future <String> uploadProfileImage(String uid, String imagePath);
-//syncronus function
-//add all feature related functions
-//add Future infront of function(for syncronus functions) palaipalo
-//blue print of all functions
+  // Discovery
+  Future<List<UserModel>> getSuggestedUsers(String currentUserId);
+  Future<List<UserModel>> searchUsers(String query);
+
+  // Profile / social
+  Future<UserModel> getCurrentUser();
+  Future<UserModel> getUserById(String uid);
+  Future<void> updateProfile(UserModel user);
+  Future<void> followUser(String targetUid);
+  Future<void> unfollowUser(String targetUid);
+  Future<void> blockUser(String targetUid);
+  Future<void> unblockUser(String targetUid);
 }
