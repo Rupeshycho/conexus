32f557c5aa798a42716c55153dc7663f338c78a9
unknown <ujwalacharya74@gmail.com>
2026-07-13 13:57:28 +0545

    adding cloudinary service

--- lib/services/cloudinary_service.dart
+++ lib/services/cloudinary_service.dart
@@ -6,28 +6,24 @@ class CloudinaryService {
   static const String cloudName = "di3lnc0c3";
   static const String uploadPreset = "conexus_upload";
 
-  static Future<String?> uploadFile(File file, {required String resourceType}) async {
+  static Future<String?> uploadFile(
+      File file, {
+        required String resourceType,
+      }) async {
     final uri = Uri.parse(
       "https://api.cloudinary.com/v1_1/$cloudName/$resourceType/upload",
     );
 
-    var request = http.MultipartRequest("POST", uri);
+    final request = http.MultipartRequest("POST", uri)
+      ..fields["upload_preset"] = uploadPreset
+      ..files.add(await http.MultipartFile.fromPath("file", file.path));
 
-    request.fields["upload_preset"] = uploadPreset;
-
-    request.files.add(
-      await http.MultipartFile.fromPath(
-        "file",
-        file.path,
-      ),
-    );
-
-    var response = await request.send();
+    final response = await request.send();
 
     if (response.statusCode == 200) {
-      var body = await response.stream.bytesToString();
-      var json = jsonDecode(body);
-      return json["secure_url"];
+      final body = await response.stream.bytesToString();
+      final json = jsonDecode(body);
+      return json["secure_url"] as String?;
     }
 
     return null;
